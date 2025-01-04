e3ba6f034c44013a9be3d5f921a991ff17a7e823
Leonard Göhrs <leonard@goehrs.eu>
2025-01-04 18:05:56 +0100

    database: start re-organization

--- taggart/database/__init__.py
+++ taggart/database/__init__.py
@@ -1,75 +1,15 @@
 #!/usr/bin/env python3
 
-import contextlib
-import functools as ft
 import itertools as it
-import operator as op
 import os
 import re
 import sqlite3
 
-import numpy as np
 import torch
 from PIL import Image
 
-from .preview_decoder import PreviewDecoder
-
-
-class TensorFile:
-    def __init__(self, path: str, shape: tuple[int], cpu=False, dtype=np.float32):
-        self.path = path
-        self.shape = shape
-        self.cpu = cpu
-        self.dtype = dtype
-
-        self._rw = None
-        self._ro = None
-
-    def resize(self, shape):
-        assert shape[1:] == self.shape[1:]
-
-        if shape[0] > self.shape[0]:
-            self.shape = shape
-            self._rw = None
-            self._ro = None
-
-    def read_write(self):
-        if self._rw is None:
-            bytes_per_elem = self.dtype(0).itemsize
-            elems_per_row = ft.reduce(op.mul, self.shape[1:])
-            bytes_per_row = bytes_per_elem * elems_per_row
-
-            min_rows = self.shape[0]
-            min_size = min_rows * bytes_per_row
-
-            with contextlib.suppress(FileExistsError), open(self.path, "x") as fd:
-                fd.close()
-
-            current_size = os.stat(self.path).st_size
-
-            assert current_size % bytes_per_row == 0
-
-            if current_size < min_size:
-                os.truncate(self.path, min_size)
-                current_size = min_size
-
-            self.shape = (current_size // bytes_per_row, *self.shape[1:])
-
-            mmap = np.memmap(self.path, self.dtype, "r+", 0, self.shape)
-            self._rw = torch.from_numpy(mmap)
-
-        # Invalidate the read only copy of the tensor that may reside
-        # on the GPU.
-        self._ro = None
-
-        return self._rw
-
-    def read_only(self):
-        if self._ro is None:
-            rw = self.read_write()
-            self._ro = rw if self.cpu else rw.cuda()
-
-        return self._ro
+from ..preview_decoder import PreviewDecoder
+from .tensor_file import TensorFile
 
 
 class Database:
